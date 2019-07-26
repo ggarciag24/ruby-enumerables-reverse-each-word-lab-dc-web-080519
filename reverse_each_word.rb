334302947b8bc3f7bc1b706bb326fab6afe89dbf
German Garcia <ggarciag.business@outlook.com>
2019-07-26 19:15:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 require('pry')
 def reverse_each_word(sentence)
     sentenceArray = sentence.split()
-    sentenceArray.collect {|n| n.reverse}
+    new = sentenceArray.collect {|n| n.reverse}
     binding.pry
-    sentenceArray.join(" ")
+    new.join(" ")
     
 end
